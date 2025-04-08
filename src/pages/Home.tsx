c05d9a020f292fed29deeaddedf6c21cf7858ed1
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-04-08 17:27:43 +0600

    hardbay initial project setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import CommonWrapper from "../common/CommonWrapper";
+
+const Home = () => {
+  return (
+    <CommonWrapper>
+      <div className="h-screen bg-website-color-lightGreen">Home page</div>
+    </CommonWrapper>
+  );
+};
+
+export default Home;
