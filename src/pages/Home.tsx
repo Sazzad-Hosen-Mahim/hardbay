43ec8c3f62cbfe24dd1d84a692c0a727d91cab42
eitty moni <eitty.t@gmail.com>
2025-04-15 16:23:35 +0600

    homepage successstory, specialise section add

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,18 @@
 import HeroSection from "@/components/homePage/HeroSection";
 import ServicesSection from "@/components/homePage/ServicesSection";
+import Specialise from "@/components/homePage/Specialise";
+import SuccessStoryBanner from "@/components/homePage/SuccessStoryBanner";
 
 const Home = () => {
-  return <div className=" bg-primary-bg">
+  return <div className=" bg-primary-bg flex flex-col gap-4">
 
     <HeroSection/>
-   
 
-    <ServicesSection/>
+  <ServicesSection/>
+
  
+    <SuccessStoryBanner/>
+ <Specialise/>
   
   </div>;
 };
