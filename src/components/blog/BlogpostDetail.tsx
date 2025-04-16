32ab7e386c4d0df2adbee5e910c882bbc8e5e758
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-04-16 17:57:09 +0600

    blog details

--- src/components/blog/BlogpostDetail.tsx
+++ src/components/blog/BlogpostDetail.tsx
@@ -17,7 +17,7 @@ const BlogPostDetail = () => {
 
   return (
     <CommonWrapper>
-      <div className="bg-white p-4 md:p-10 mx-auto flex flex-col md:flex-row gap-10">
+      <div className="bg-white p-4 md:p-10 mx-auto flex flex-col md:flex-row gap-10 mt-16">
         {/* Left column: Blog Post Detail */}
         <div className="flex-1 space-y-6">
           <h1 className="text-3xl md:text-4xl font-bold mb-6 text-primary-blue">
@@ -29,7 +29,9 @@ const BlogPostDetail = () => {
 
         {/* Right column: Blog Headlines (auto-height) */}
         <div className="w-full md:w-[300px] space-y-6 border border-gray-300 rounded-2xl shadow-md p-4 self-start">
-          <h2 className="text-2xl font-semibold text-primary-orange">Headlines</h2>
+          <h2 className="text-2xl font-semibold text-primary-orange">
+            Headlines
+          </h2>
           <ul className="space-y-3">
             {blogPosts.map((post) => (
               <li
