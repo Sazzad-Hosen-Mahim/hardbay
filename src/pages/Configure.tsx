fc904399164b026a2e66a407921875ccc883753b
eitty moni <eitty.t@gmail.com>
2025-04-19 16:43:06 +0600

    configure add

--- src/pages/Configure.tsx
+++ src/pages/Configure.tsx
@@ -1,8 +1,11 @@
+import ConfigureComponent from "@/components/configure/ConfigureComponent"
 
 
 const Configure = () => {
   return (
-    <div className="pt-16">Configure</div>
+    <div className="pt-16">
+        <ConfigureComponent/>
+    </div>
   )
 }
 
